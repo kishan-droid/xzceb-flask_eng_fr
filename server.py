8a90e424179244e4bcff9a7beff1d1166306dfa2
kishan-droid <kishan21bcs192@iiitkottayam.ac.in>
2023-07-24 23:13:34 +0530

    hola

---
+++ server.py
@@ -0,0 +1,27 @@
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    translator.english_to_french(textToTranslate)
+    return "Translated text to French"
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translator.french_to_english(textToTranslate)
+    return "Translated text to English"
+
+@app.route("/")
+def renderIndexPage():
+    with open('index.html', 'r') as file:
+        file_content = file.read()
+    print(file_content)
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
