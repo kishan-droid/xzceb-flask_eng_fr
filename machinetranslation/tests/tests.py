27b100cdf4cb50f2916a278ef7fc19f019facfd1
kishan-droid <kishan21bcs192@iiitkottayam.ac.in>
2023-07-25 00:42:33 +0530

    Final

--- machinetranslation/tests/tests.py
+++ machinetranslation/tests/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from machinetranslation.translator import english_to_french, french_to_english
+
 class test_english_to_french(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
